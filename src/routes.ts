9a6d903a20f5d9d0beabbd392899ca67427228c0
Igho matthew <Matthew.Igho@vfdtech.ng>
2021-03-01 18:47:08 +0100

    Adds security

--- src/routes.ts
+++ src/routes.ts
@@ -2,18 +2,16 @@ import { Router } from 'express';
 import FixedController from './app/controllers/FixedController';
 import RandomController from './app/controllers/RandomController';
 import UserController from './app/controllers/UserController';
+import verifyToken from './app/middlewares/verifyToken';
 
 const apiV1Router = Router();
-
 
 // Auth
 apiV1Router.post('/auth/login', UserController.login);
 apiV1Router.post('/auth/register', UserController.register);
 
-
-apiV1Router.get('/jokes', FixedController.getJokes);
-apiV1Router.get('/anime', RandomController.getAnime);
-
-
+// resource
+apiV1Router.get('/jokes',verifyToken, FixedController.getJokes);
+apiV1Router.get('/anime',verifyToken, RandomController.getAnime);
 
 export default apiV1Router;
