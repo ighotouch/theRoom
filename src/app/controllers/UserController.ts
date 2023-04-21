7b343b5210d6b2b9574d2cd1cc57e21f2dce9656
Matthew Igho <ighotouch@gmail.com>
2023-04-21 10:59:51 +0200

    Update UserController.ts
    Optimize code

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -1,53 +1,58 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
-import models from "../../database/models/index";
+import { User } from "../../database/models";
 import { IRegisterRequest } from "../interfaces/request";
 import {
   registrationRequestSchema,
   loginRequestSchema,
 } from "../validators/userSchema";
-import "../../database/config/dotenv";
-const { User } = models;
+import dotenv from "dotenv";
 
-const secret = process.env.JWT_SECRET;
+dotenv.config();
+const secret = process.env.JWT_SECRET as string;
 
 class UserController {
-  static async register(req: IRegisterRequest, res: express.Response) {
+  static async register(req: Request<IRegisterRequest>, res: Response) {
     try {
       const value = await registrationRequestSchema.validateAsync(req.body);
-      const resp = await User.create(req.body);
-
-      return res.status(200).json({
+      await User.create(req.body);
+      res.status(200).json({
         message: "Registration successful",
       });
     } catch (err) {
-      return res.status(400).json({
+      res.status(400).json({
         message: err.message,
       });
     }
   }
 
-  static async login(req: express.Request, res: any) {
+  static async login(req: Request, res: Response) {
     try {
       const value = await loginRequestSchema.validateAsync(req.body);
-      const usr = await User.findOne({ where: { email: req.body.email } });
+      const user = await User.findOne({ where: { email: req.body.email } });
 
-      if (!usr) return res.status(404).send("No user found.");
-
-      if (!usr || !bcrypt.compareSync(req.body.password, usr.password)) {
-        return res
-          .status(401)
-          .send({ auth: false, token: null, message: "Invalid Credentials" });
+      if (!user) {
+        return res.status(404).send("No user found.");
       }
 
-      var token = jwt.sign({ id: usr.id }, secret, {
-        expiresIn: 86400, // expires in 24 hours
+      if (!bcrypt.compareSync(req.body.password, user.password)) {
+        return res.status(401).send({
+          auth: false,
+          token: null,
+          message: "Invalid credentials",
+        });
+      }
+
+      const token = jwt.sign({ id: user.id }, secret, {
+        expiresIn: "24h",
       });
 
-      return res
-        .status(200)
-        .send({ auth: true, token: token, message: "success" });
+      res.status(200).json({
+        auth: true,
+        token: token,
+        message: "Login successful",
+      });
     } catch (err) {
       res.status(400).json({
         message: err.message,
