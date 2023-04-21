4086f7b2388992ae83e7e50b2f2917691e33749a
Matthew Igho <ighotouch@gmail.com>
2023-04-21 10:57:10 +0200

    Update routes.ts
    Moved validation to a middle ware

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,37 @@
 import { Router } from 'express';
+import Joi from 'joi';
 import FixedController from './app/controllers/FixedController';
 import RandomController from './app/controllers/RandomController';
 import UserController from './app/controllers/UserController';
 import verifyToken from './app/middlewares/verifyToken';
 
+
 const apiV1Router = Router();
 
+export const validateSchemaMiddleWare =
+    (schema: Joi.ObjectSchema<any>) =>
+    (
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+    ) => {
+        const { error, value } = schema.validate({
+            ...req.body,
+            ...req.params,
+            ...req.query
+        });
+        if (error) {
+            return error.details.forEach((e) => {
+                res.status(400).json({
+                    message: e.message.replace(/['"]/g, '')
+                });
+            });
+        }
+        next();
+    };
+
 // Auth
-apiV1Router.post('/auth/login', UserController.login);
+apiV1Router.post('/auth/login', validateSchemaMiddleWare(authLoginSchema), UserController.login);
 apiV1Router.post('/auth/register', UserController.register);
 
 // resource
